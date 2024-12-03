0f971c9a7975b3d6f9f7ea9d47515634effc9e31
Dylan Hollemaert <d40598408@gmail.com>
2024-12-03 22:21:06 +0100

    Ajout d'une fenêtre qui peut se fermer

---
+++ Food'estroyer/Food'estroyer/Dylan/game.h
@@ -0,0 +1,37 @@
+#ifndef GAME_H
+#define GAME_H
+
+// --- STL --- //
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iomanip>
+// --- SFML --- //
+#include "SFML/System.hpp"
+#include "SFML/Audio.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
+
+class Game {
+public:
+
+	Game();
+
+	void run();
+
+private:
+
+	bool m_isRunning;
+	sf::WindowBase windowBase;
+	sf::RenderWindow window;
+	
+private:
+	void setupGraphicalElements();
+	void pollEvents();
+	void update();
+	void render();
+
+};
+
+#endif // !GAME_H
