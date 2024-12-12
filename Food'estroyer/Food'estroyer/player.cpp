18dab0f4a5f2887f264aeb3237c6aaca3056ddd6
JulienRdgs <julien.rodrigues.onze@gmail.com>
2024-12-12 12:04:45 +0100

    moitié commit update player (moitié du game.cpp manquante)

--- Food'estroyer/Food'estroyer/player.cpp
+++ Food'estroyer/Food'estroyer/player.cpp
@@ -1,19 +1,26 @@
 #include "game.h"
 #include "player.h"
 
-std::vector<sf::CircleShape> vectorPies;
 
 Player::Player() : hp(100), speed(20), atkPower(10) {
 	this->setRadius(50);
 	this->setFillColor(sf::Color::Green);
 }
 
-void Player::throwPie() { // Ajouter le cooldown dans les KeyPressed ?
-						  // Pourquoi pas, dans tout les cas il s'agira d'une durée de temps donc n'importe où ça peut contôler une durée de temps.
-	sf::CircleShape pie(10);                                        // Quel rayon ?
-																	// Dylan : Pas en terme de pixels, mais plutôt en ratio de la taille de la fenêtre, je dirais 1% de la largeur de la fenêtre
-																	// En gros, 50 tartes collées côte à côte feraient l'ensemble de la largeur de l'écran.
-	//pie.setTexture();
-	pie.setPosition(getPosition().x + pie.getRadius() * 2, getPosition().y);
-	vectorPies.push_back(pie);  // Pour faire les collisions des pies et leurs mouvements
+void Player::throwPie(std::vector<Pie*> &vectorPies, sf::RenderWindow &window, float timeElapsed) {
+	Pie* pie = new Pie;
+	pie->setAtkPower(atkPower);
+	pie->setRadius(getPieSize() * window.getSize().x);
+	pie->setSpeed(getPieSpeed());
+	pie->setPosition(getPosition().x + getRadius() + pie->getRadius(), getPosition().y + getRadius() - pie->getRadius());
+	vectorPies.push_back(pie);
+	shootCooldown = 0;
 }
+
+// Ajouter le cooldown dans les KeyPressed ?
+// Pourquoi pas, dans tout les cas il s'agira d'une durée de temps donc n'importe où ça peut contôler une durée de temps.
+
+// Quel rayon ?
+// Dylan : Pas en terme de pixels, mais plutôt en ratio de la taille de la fenêtre, je dirais 1% de la largeur de la fenêtre
+// En gros, 50 tartes collées côte à côte feraient l'ensemble de la largeur de l'écran.
+
--- Food'estroyer/Food'estroyer/player.h
+++ Food'estroyer/Food'estroyer/player.h
@@ -3,46 +3,60 @@
 
 #include "game.h"
 
+class Pie : public sf::CircleShape {
+private:
+
+	bool state = true;
+	float atkPower = 1;
+	float speed;
+	float size;
+
+public:
+
+	float getState() const    { return state; }
+	float getAtkPower() const { return atkPower; }
+	float getSpeed() const    { return speed; }
+	float getSize() const     { return size; }
+
+	void setState(float newState) { state = newState; }
+	void setAtkPower(float newAtkPower) { atkPower = newAtkPower; }
+	void setSpeed(float newSpeed)       { speed = newSpeed; }
+	void setSize(float newSize)         { size = newSize; }
+};
+
 class Player : public sf::CircleShape {
 private:
 	bool alive = true;
 	float hp;
 	float speed;
 	float atkPower;
+	float pieSpeed = 1000;																			 ////////////// PIE SPEED HERE
+	float pieSize = 0.01;
+	float shootCooldown = 0;
 
 public:
 	
 	Player();
 
-	void throwPie();
+	void throwPie(std::vector<Pie*> &vectorPies, sf::RenderWindow &window, float timeElapsed);
 
 	// Allez un peu d'encapsulation :)
 	// Getters
-	bool getPlayerLife() const {
-		return alive;
-	}
-	float getPlayerHP() const {
-		return hp;
-	}
-	float getSpeed() const {
-		return speed;
-	}
-	float getAtkPower() const {
-		return atkPower;
-	}
+	bool getPlayerLife() const     { return alive; }
+	float getPlayerHP() const      { return hp; }
+	float getSpeed() const         { return speed; }
+	float getAtkPower() const      { return atkPower; }
+	float getPieSpeed() const      { return pieSpeed; }
+	float getPieSize() const       { return pieSize; }
+	float getShootCooldown() const { return shootCooldown; }
 	// Setters
-	void damagePlayer(float degats) {
-		hp -= degats;
-	}
-	void setPlayerSpeed(int bonusSpeed) { // Bonus speed entre 0 et 100 comme un pourcentage
-		speed *= (0.01f * bonusSpeed);
-	}
-	void setPlayerSpeedBackToNormal(int bonusSpeed) { // Opération inverse
-		speed /= (0.01f * bonusSpeed);
-	}
-	void setAtkPower(float atkMultiplier) { // Genre 1.2x, 1.55x etc...
-		atkPower *= atkMultiplier;
-	}
+	void damagePlayer(float degats)						{ hp -= degats; }
+	void setPlayerSpeed(int bonusSpeed)					{ speed *= (0.01f * bonusSpeed); }// Bonus speed entre 0 et 100 comme un pourcentage
+	void setPlayerSpeedBackToNormal(int bonusSpeed)		{ speed /= (0.01f * bonusSpeed); }// Opération inverse
+	void setAtkPower(float atkMultiplier)				{ atkPower *= atkMultiplier; } // Genre 1.2x, 1.55x etc...
+	void setPieSpeed(float newSpeed) { pieSpeed = newSpeed; }
+	void setPieSize(float newSize) { pieSize = newSize; }
+	void setShootCooldown(float timeElapsed) { shootCooldown += timeElapsed; }
 
 };
 #endif
